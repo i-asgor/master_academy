0f3b80669392016bedcadeb7b1ede82255c9193f
Md Asgor Office <asgor.ice@gmail.com>
2021-03-01 15:42:55 +0600

    Mathmatical Operation

---
+++ Chapter_Two/mathdemo/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	a := 2.5
+	b := 3.2
+
+	c := math.Pow(a, 2)
+	fmt.Printf("%.2f ^ %d = %.2f\n", a, 2, c)
+
+	d := math.Sin(a)
+	fmt.Printf("Sin(%.2f) = %.2f\n", a, d)
+
+	e := math.Cos(b)
+	fmt.Printf("Cos(%.2f) = %.2f\n", b, e)
+}
