e8cfe9c1e847aef771e3b6db33de0e3c023284e8
Md Asgor Office <asgor.ice@gmail.com>
2021-03-01 14:35:02 +0600

    variable declare then value assign

---
+++ Chapter_Two/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var str string
+	var n, m int
+	var mn float32
+
+	str = "Md. Asgor"
+	n = 10
+	m = 20
+	mn = 25.3
+
+	fmt.Println("value of str", str)
+	fmt.Println("value of n", n)
+	fmt.Println("value of m", m)
+	fmt.Println("value of mn", mn)
+}
