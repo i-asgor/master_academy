496134697642cc49a43c93254ddcb39bd437cb7f
Md Asgor Office <asgor.ice@gmail.com>
2021-03-03 15:08:20 +0600

    Addition Two value

---
+++ codewars/addition/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 20
+	c := a + b
+	fmt.Println("Addition", c)
+
+}
--- codewars/string_reverse.go
+++ codewars/string_reverse.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func Solution(word string) string {
-    runes := []rune(word)
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
+	runes := []rune(word)
+	fmt.Println(runes)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
-func main(){
+func main() {
 	fmt.Println(Solution("this is asgor"))
 }
