115a799ddc0d23168f28f41408c60d7f451c9677
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2021-03-06 17:24:53 +0600

    webserver initialization

---
+++ Class_9/first/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+//type Handler interface{
+//	ServeHTTP(ResponseWriter, *Request)
+//}
+
+func main() {
+
+	//var name Datatype
+	//var x string
+	//var handler func(ResponseWriter,*Request)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/contact", contact)
+	http.HandleFunc("/about", about)
+	http.ListenAndServe(":8080", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Welcome to my first golang webpage with Coding Bootcamp`)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Welcome to Coding Bootcamp contact page`)
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Welcome to Coding Bootcamp about page`)
+}
