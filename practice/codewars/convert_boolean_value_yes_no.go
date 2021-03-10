089caef9ff18403f8b2061fdefa3bab1b483c97f
Md Asgor Office <asgor.ice@gmail.com>
2021-03-10 17:32:13 +0600

    convert voolean value yes or no

---
+++ practice/codewars/convert_boolean_value_yes_no.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func BoolToWord(word bool) string {
+  if word{
+    return "Yes"
+  }else{
+    return "No"
+  }
+}
+
+func main(){
+	abcd = "This is Asgor"
+	fmt.Println(BoolToWord())
+}
