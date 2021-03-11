c38f03133c081af68c01d4de5ff9dfd5b94b052e
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2021-03-12 00:16:14 +0600

    string value pass

--- template_practice/main.go
+++ template_practice/main.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	ptmp, err := template.ParseFiles("two.html", "one.html")
+	ptmp, err := template.ParseFiles("one.html", "two.html")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	ptmp.Execute(os.Stdout, nil)
+	ptmp.Execute(os.Stdout, "asgor")
 }
