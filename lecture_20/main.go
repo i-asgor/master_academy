6b164fa703e22536de7bee9b73999f44b23260d8
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2021-03-17 00:49:25 +0600

    data insert from web form to database without redirect

--- lecture_20/main.go
+++ lecture_20/main.go
@@ -1,56 +1,69 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 
-	_ "github.com/go-sql-driver/mysql" //underscore means building package or building method handle
+	"github.com/mateors/mcb"
 )
 
-var db *sql.DB
+//var db *sql.DB
+var db *mcb.DB
 var err error
 
 func init() {
+
 	// Open up our database connection.
 	// I've set up a database on my local machine using phpmyadmin.
 	// The database is called testDb
-	db, err = sql.Open("mysql", "root:iasgor@tcp(127.0.0.1:3306)/hosting_db")
+	//db, err = sql.Open("mysql", "root:test123@tcp(127.0.0.1:3306)/hosting_db")
 
 	// if there is an error opening the connection, handle it
-	if err != nil {
-		panic(err.Error())
-	}
-
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
 	// defer the close till after the main function has finished
 	// executing
-	// defer db.Close()
+	//defer db.Close()
+	//fmt.Println("db connection successful")
 
-	fmt.Println("db connection successfully")
+	//couchbase connection block
+	db = mcb.Connect("localhost", "asgor", "iasgor")
+	res, err := db.Ping()
+	if err != nil {
+
+		fmt.Println(res)
+		os.Exit(1)
+	}
+	fmt.Println(res, err)
 }
 
 func main() {
 
 	http.HandleFunc("/", home)
 	http.HandleFunc("/request", request)
+	//http.HandleFunc("/requestc", requestc)
 	http.HandleFunc("/features", features)
 	http.HandleFunc("/docs", docs)
-	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer((http.Dir("assets")))))
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("assets"))))
 	http.ListenAndServe(":8888", nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+
 	ptmp, err := template.ParseFiles("template/base.gohtml")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	ptmp.Execute(w, nil)
-
+	//fmt.Fprintf(w, `welcome`)
 }
 
 func features(w http.ResponseWriter, r *http.Request) {
+
 	ptmp, err := template.ParseFiles("template/base.gohtml")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -60,12 +73,12 @@ func features(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 	ptmp.Execute(w, nil)
-
+	//fmt.Fprintf(w, `welcome`)
 }
 
 func docs(w http.ResponseWriter, r *http.Request) {
+
 	ptmp, err := template.ParseFiles("template/base.gohtml")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -77,34 +90,71 @@ func docs(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ptmp.Execute(w, nil)
+	//fmt.Fprintf(w, `welcome`)
+}
 
+// func request(w http.ResponseWriter, r *http.Request) {
+
+// 	//method-1
+// 	name := r.FormValue("name")
+// 	company := r.FormValue("company")
+// 	email := r.FormValue("email")
+
+// 	// fmt.Println(name, company, email)
+// 	// fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
+
+// 	//method-2
+// 	// r.ParseForm()
+// 	// for key, val := range r.Form {
+// 	// 	fmt.Println(key, val)
+// 	// }
+
+// 	qs := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL, '%s', '%s', '%s', '1');"
+// 	sql := fmt.Sprintf(qs, name, company, email)
+// 	//fmt.Println(sql)
+// 	insert, err := db.Query(sql)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	defer insert.Close()
+
+// 	fmt.Fprintf(w, `OK`)
+// }
+
+//like mysql table schema
+type RequestTable struct {
+	ID      string `json:"aid"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Company string `json:"company"`
+	Type    string `json:"type"`
+	Status  int    `json:"status"`
 }
 
 func request(w http.ResponseWriter, r *http.Request) {
 
-	//method 1
-	name := r.FormValue("name")
-	company := r.FormValue("company")
-	email := r.FormValue("email")
+	//method-1
+	// name := r.FormValue("name")
+	// company := r.FormValue("company")
+	// email := r.FormValue("email")
 
 	// fmt.Println(name, company, email)
+	// fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
 
-	// fmt.Fprintf(w, "Received %s %s %s", name, company, email) //response
-
-	//method 2
-	// r.ParseForm()
-	// for key, val := range r.Form {
-	// 	fmt.Println(key, val) // key = string and value = slice of string
-	// }
-	qs := "INSERT INTO `request` (`id`,`name`,`company`,`email`,`status`) VALUES (NULL, '%s', '%s','%s','1')"
-	sql := fmt.Sprintf(qs, name, company, email)
-	insert, err := db.Query(sql)
-
-	// if there is an error inserting, handle it
-	if err != nil {
-		panic(err.Error())
+	//method-2
+	r.ParseForm()
+	for key, val := range r.Form {
+		fmt.Println(key, val)
 	}
-	// be careful deferring Queries if you are using transactions
-	defer insert.Close()
-	fmt.Fprintf(w, "ok")
+
+	var reqTable RequestTable
+
+	r.Form.Add("bucket", "master_academy")
+	r.Form.Add("aid", "request::7") //we will update later
+	r.Form.Add("type", "request")
+	r.Form.Add("status", "1")
+	pRes := db.Insert(r.Form, &reqTable)
+	fmt.Println(pRes.Status, pRes.Errors)
+
+	fmt.Fprintf(w, `OK`)
 }
