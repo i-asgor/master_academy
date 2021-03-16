5b6c4b565514e7dc64904861b2d5e8ecd6507015
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2021-03-16 23:52:35 +0600

    data insert from webform to couchbase database

--- lecture_20/mcb/main.go
+++ lecture_20/mcb/main.go
@@ -2,26 +2,35 @@ package main
 
 import (
 	"fmt"
-	"net/url"
+	"html/template"
+	"net/http"
 	"os"
 
 	"github.com/mateors/mcb"
 )
 
+//var db *sql.DB
 var db *mcb.DB
-
-type RequstTable struct {
-	ID      string `json:"aid"`
-	Name    string `json:"name"`
-	Company string `json:"company"`
-	Email   string `json:"email"`
-	Type    string `json:"type"`
-	Status  int    `json:"status"`
-}
+var err error
 
 func init() {
 
-	db = mcb.Connect("localhost", "asgor", "iasgor")
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	//db, err = sql.Open("mysql", "root:test123@tcp(127.0.0.1:3306)/hosting_db")
+
+	// if there is an error opening the connection, handle it
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// defer the close till after the main function has finished
+	// executing
+	//defer db.Close()
+	//fmt.Println("db connection successful")
+
+	//couchbase connection block
+	db = mcb.Connect("localhost", "mostain", "test123")
 	res, err := db.Ping()
 	if err != nil {
 
@@ -29,25 +38,123 @@ func init() {
 		os.Exit(1)
 	}
 	fmt.Println(res, err)
-
 }
 
 func main() {
 
-	//How to insert into couchbase bucket
-	var myData RequstTable
+	http.HandleFunc("/", home)
+	http.HandleFunc("/request", request)
+	//http.HandleFunc("/requestc", requestc)
+	http.HandleFunc("/features", features)
+	http.HandleFunc("/docs", docs)
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("assets"))))
+	http.ListenAndServe(":8888", nil)
+}
 
-	form := make(url.Values, 0)
-	form.Add("bucket", "master_academy") 
-	form.Add("aid", "request::3")               
-	form.Add("name", "Md shumon")
-	form.Add("company", "Master Academy")
-	form.Add("email", "shumon@gmail.com")
-	form.Add("type", "request") 
-	form.Add("status", "1")
+func home(w http.ResponseWriter, r *http.Request) {
 
-	p := db.Insert(form, &myData)    
-	fmt.Println("Status:", p.Status) 
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
+	ptmp.Execute(w, nil)
+	//fmt.Fprintf(w, `welcome`)
+}
 
+func features(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp, err = ptmp.ParseFiles("wpage/features.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+	//fmt.Fprintf(w, `welcome`)
+}
+
+func docs(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp, err = ptmp.ParseFiles("wpage/docs.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp.Execute(w, nil)
+	//fmt.Fprintf(w, `welcome`)
+}
+
+// func request(w http.ResponseWriter, r *http.Request) {
+
+// 	//method-1
+// 	name := r.FormValue("name")
+// 	company := r.FormValue("company")
+// 	email := r.FormValue("email")
+
+// 	// fmt.Println(name, company, email)
+// 	// fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
+
+// 	//method-2
+// 	// r.ParseForm()
+// 	// for key, val := range r.Form {
+// 	// 	fmt.Println(key, val)
+// 	// }
+
+// 	qs := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL, '%s', '%s', '%s', '1');"
+// 	sql := fmt.Sprintf(qs, name, company, email)
+// 	//fmt.Println(sql)
+// 	insert, err := db.Query(sql)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	defer insert.Close()
+
+// 	fmt.Fprintf(w, `OK`)
+// }
+
+//like mysql table schema
+type RequestTable struct {
+	ID      string `json:"aid"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Company string `json:"company"`
+	Type    string `json:"type"`
+	Status  int    `json:"status"`
+}
+
+func request(w http.ResponseWriter, r *http.Request) {
+
+	//method-1
+	// name := r.FormValue("name")
+	// company := r.FormValue("company")
+	// email := r.FormValue("email")
+
+	// fmt.Println(name, company, email)
+	// fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
+
+	//method-2
+	r.ParseForm()
+	for key, val := range r.Form {
+		fmt.Println(key, val)
+	}
+
+	var reqTable RequestTable
+
+	r.Form.Add("bucket", "master_academy")
+	r.Form.Add("aid", "request::6") //we will update later
+	r.Form.Add("type", "request")
+	r.Form.Add("status", "1")
+	pRes := db.Insert(r.Form, &reqTable)
+	fmt.Println(pRes.Status, pRes.Errors)
+
+	fmt.Fprintf(w, `OK`)
 }
