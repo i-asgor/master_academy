d25554e8ab695a214a52c2cb5aa7097a4e1823e0
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2021-04-03 02:04:03 +0600

    vat calculation

---
+++ lecture_28/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"master_academy/lecture_28/vatcalculator"
+)
+
+func main() {
+	i := vatcalculator.InclusiveTax(200, 15)
+	fmt.Println("Inclusive tax: ", i)
+
+	e := vatcalculator.ExclusiveTax(200, 15)
+	fmt.Println("Exclusive tax : ", e)
+}
