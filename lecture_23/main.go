22716b5ff5ad00edfa6f22ded3ff8562dcb55ba3
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2021-04-05 18:48:44 +0600

    string number add using for range loop

--- lecture_23/main.go
+++ lecture_23/main.go
@@ -9,8 +9,17 @@ func main() {
 	result := Sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 	fmt.Println("All Int value : ", result)
 
-	result2 := Sum2(1, 2, 3, 4, 5, "6", 7, 8, "9", 10)
-	fmt.Println("All type Number value : ", result2)
+	// result2 := Sum2(1, 2, 3, 4, 5, "6", 7, 8, "9", 10)
+	// fmt.Println("All type Number value : ", result2)
+
+	strSlc := []string{"10", "20", "30"}
+	var res int64
+	for i, slc := range strSlc {
+
+		res += Sum2(slc)
+		fmt.Println(i, res)
+	}
+	fmt.Println("str number add : ", res)
 }
 
 func Sum2(nums ...interface{}) int64 {
