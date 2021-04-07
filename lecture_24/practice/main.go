a573c8403b1603bf2a3f8547df085a41589f2f50
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2021-04-07 07:04:42 +0600

    Excel bar diagram create

--- lecture_24/practice/main.go
+++ lecture_24/practice/main.go
@@ -62,25 +62,68 @@ func main() {
 	// 	fmt.Println(err)
 	// }
 
-	f, err := excelize.OpenFile("Book1.xlsx")
-	if err != nil {
+	// f, err := excelize.OpenFile("Book1.xlsx")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// // Get value from cell by given worksheet name and axis.
+	// cell, err := f.GetCellValue("Sheet1", "B2")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(cell)
+	// // Get all the rows in the Sheet1.
+	// rows, err := f.GetRows("Sheet2")
+	// for _, row := range rows {
+	// 	for _, colCell := range row {
+	// 		fmt.Print(colCell, "\t")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	categories := map[string]string{
+		"A2": "Small", "A3": "Normal", "A4": "Large",
+		"B1": "Apple", "C1": "Orange", "D1": "Pear"}
+	values := map[string]int{
+		"B2": 2, "C2": 3, "D2": 3, "B3": 5, "C3": 2, "D3": 4, "B4": 6, "C4": 7, "D4": 8}
+	f := excelize.NewFile()
+	for k, v := range categories {
+		f.SetCellValue("Sheet1", k, v)
+	}
+	for k, v := range values {
+		f.SetCellValue("Sheet1", k, v)
+	}
+	if err := f.AddChart("Sheet1", "E1", `{
+        "type": "col3DClustered",
+        "series": [
+        {
+            "name": "Sheet1!$A$2",
+            "categories": "Sheet1!$B$1:$D$1",
+            "values": "Sheet1!$B$2:$D$2"
+        },
+        {
+            "name": "Sheet1!$A$3",
+            "categories": "Sheet1!$B$1:$D$1",
+            "values": "Sheet1!$B$3:$D$3"
+        },
+        {
+            "name": "Sheet1!$A$4",
+            "categories": "Sheet1!$B$1:$D$1",
+            "values": "Sheet1!$B$4:$D$4"
+        }],
+        "title":
+        {
+            "name": "Fruit 3D Clustered Column Chart"
+        }
+    }`); err != nil {
 		fmt.Println(err)
 		return
 	}
-	// Get value from cell by given worksheet name and axis.
-	cell, err := f.GetCellValue("Sheet1", "B2")
-	if err != nil {
+	// Save spreadsheet by the given path.
+	if err := f.SaveAs("Book1.xlsx"); err != nil {
 		fmt.Println(err)
-		return
-	}
-	fmt.Println(cell)
-	// Get all the rows in the Sheet1.
-	rows, err := f.GetRows("Sheet2")
-	for _, row := range rows {
-		for _, colCell := range row {
-			fmt.Print(colCell, "\t")
-		}
-		fmt.Println()
 	}
 
 }
